12806102a8041acbeccaa3ff0631dccb58955a8e
Muhammed Adil C.P <muhammedadil.cp@wavetronix.com>
2026-09-16 22:24:34 +0530

    inheritance programs added

---
+++ LibraryMt/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using LibraryMt;
+
+library l1=new library();
+l1.Bookname = "C#"; 
+l1.AuthorName = "John Doe"; 
+l1.Serialno = 12345;  
+l1.PublishingDate = new DateTime(2020, 1, 1, 10, 0, 0);
+
+library l2 =new library("Python Programming", 10, 67890, "Jane Smith", new DateTime(2019, 6, 1));
+
+
+Console.WriteLine($"Book Name: {l1.Bookname}, Author: {l1.AuthorName}, Serial No: {l1.Serialno}, Publishing Date: {l1.PublishingDate}, Country: {library.Country}");
+
+l2.PrintBookDetails();
+l1.PrintBookDetails();
+string AuthorAndTitle = l2.PrintAuthorAndTitle();
+Console.WriteLine("Author and Title 2: " + AuthorAndTitle);
+AuthorAndTitle = l1.PrintAuthorAndTitle();
+Console.WriteLine("Author and Title 1: " + AuthorAndTitle);
+Console.WriteLine("Enter dollar value: ");
+string dollar_val= Console.ReadLine();
+float dollarval = float.Parse(dollar_val);
+float rupee_value = l2.SetPriceinDollar(dollarval);
+Console.WriteLine("Rupee Value: " + rupee_value);
+Console.ReadLine();
---
+++ LibraryMt/library.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace LibraryMt
+{
+    internal class library
+    {
+        public string Bookname { get; set; }
+        public int Serialno { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime PublishingDate { get; set; }
+        public const string Country = "India";
+
+        public float price { get; set; } = 0f;
+
+
+        public library()
+        {
+            PublishingDate = new DateTime(2020, 1, 1, 10, 0, 0);
+        }
+
+        public library(string Book_name, int _price, int Serial_No, string Author_Name, DateTime Publishing_Date) {
+            this.Bookname = Book_name;
+            this.Serialno = Serial_No;
+            this.AuthorName= Author_Name;
+            this.PublishingDate = Publishing_Date;
+            this.price = _price;    
+
+        }
+        public void PrintBookDetails()
+        {
+            Console.WriteLine($"Book Name: {this.Bookname}, Author: {this.AuthorName}, Serial No: {this.Serialno}, Publishing Date: {this.PublishingDate}, Country: {Country}");
+        }
+
+        public string PrintAuthorAndTitle() {
+            string AuthorAndTitle = this.AuthorName + " - " + this.Bookname;
+            return AuthorAndTitle;
+        }
+
+        public float SetPriceinDollar(float dollar_value) {
+            float rupee_value = dollar_value * price;
+            return rupee_value;
+        }
+    }
+}
