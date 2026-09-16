e44d5c48f627f31920f5fa8891f43064c738a4aa
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:50:35 +0530

    Initial commit

---
+++ FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using FirstApp;
+
+Console.WriteLine("Hello, World!");
+
+Student s1 = new Student();
+s1.rollNo = 1;
+s1.name = "Dona";
+s1.height = 156.0f;
+
+Console.WriteLine("rollNo:"+s1.rollNo);
+Console.WriteLine("Name:"+s1.name);
+Console.WriteLine("Height:"+s1.height);
+
+
+
+Console.ReadLine();
---
+++ FirstApp/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    internal class Student
+    {
+        public int rollNo;
+        public string name;
+         public float height;
+    }
+}
