3c935d571be350780e7fe3107228d889167d7cef
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:52:36 +0530

    Initial commit

---
+++ Library/Book.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    internal class Book
+    {
+        public int SerialNo { get; set; }
+
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public const string Country = "India";
+        public static string Location { get; set; } = "Kerala";
+
+        public Book()
+        {
+            PublishedAt = DateTime.Now;
+        }
+        public void setLocation()
+        {
+            Location = "Tamil Nadu";
+        }
+        public Book(int serial_no , string author_name,string title,int price,DateTime published_at)
+        {
+            this.SerialNo = serial_no;
+            this.AuthorName = author_name;
+            this.Title = title;
+            this.Price = price;
+            this.PublishedAt = published_at;
+
+
+        }
+        // to display details(no retun val nd para)
+        public void Display()
+        {
+            Console.WriteLine("SerialNo:" + this.SerialNo);
+            Console.WriteLine($"AuthorName:{this.AuthorName}");
+            Console.WriteLine($"Title:{this.Title}");
+
+            Console.WriteLine($"Price:{this.Price}");
+            Console.WriteLine($"PublishedAt {this.PublishedAt}");
+            Console.WriteLine($"The region is:{Book.Country}");
+            Console.WriteLine($"The region is:{Book.Location}");
+
+        }
+        // To get the author name and Tiltle(with return no para)
+        public string GetAuthorandTiltle()
+        {
+            string result = this.AuthorName + "" + this.Title;
+            return result;
+        }
+        // to set price(with return nd para)
+        public float SetPriceInDollor(float dollar_value)
+        {
+            float price_in_rupees = dollar_value * this.Price;
+            return price_in_rupees;
+        }
+
+
+
+
+
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,37 @@
+﻿using Library;
+Book b1 = new Book();
+b1.SerialNo = 101;
+b1.AuthorName = "Dona Rose Shiju";
+b1.Title = "Bright Sky";
+b1.Price = 456;
+//b1.PublishedAt = DateTime.Now;
+/*Console.WriteLine("SerialNo:" + b1.SerialNo);
+Console.WriteLine("AuthorName:" + b1.AuthorName);
+Console.WriteLine("Title:" + b1.Title);
+
+Console.WriteLine("Price:" + b1.Price);
+Console.WriteLine("PublishedAt" + b1.PublishedAt);
+Console.WriteLine("Country:" + Book.Country);*/
+b1.Display();
+string author_title = b1.GetAuthorandTiltle();
+Console.WriteLine($"the author and title:{author_title}");
+Console.WriteLine();
+
+Book b2 = new Book(102,"Sara Joseph","Rich Dad Poor Dad",600,DateTime.Now);
+b2.setLocation();
+b2.Display();
+author_title = b2.GetAuthorandTiltle();
+Console.WriteLine($"the author and title:{author_title}");
+Console.WriteLine("enter the dollar value");
+string dollar_value = Console.ReadLine();
+float value = float.Parse(dollar_value);
+
+float price_rupees = b2.SetPriceInDollor(value);
+Console.WriteLine($"the price in rupees is :{price_rupees}");
+
+Console.ReadLine();
+
+
+
+
+
