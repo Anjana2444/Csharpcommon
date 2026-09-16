fa0a78e0d9f022685224479c07752fb694e8d04d
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:53:00 +0530

    Initial commit

---
+++ MovieRating/MovieRanking.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRating
+
+{
+    internal enum MovieRatings { Excellent, Average, Bad, Poor }
+    internal class MovieRanking
+    {
+        public int MovieId { get; set; }
+
+        public string MovieName { get; set; }
+
+        public int Price { get; set; }
+
+        public MovieRatings Ratings { get; set; }
+    
+
+
+     public MovieRanking(int id, string name,int price,MovieRatings rating)
+        {
+            this.MovieId = id;
+            this.MovieName = name;
+            this.Price = price;
+            this.Ratings = rating;
+        }
+    public void DiplayDetails()
+        {
+            Console.WriteLine($"Movie Id:{MovieId}");
+            Console.WriteLine($"Movie Name:{MovieName}");
+            Console.WriteLine($"Price:{Price}");
+            Console.WriteLine($"Ratings:{Ratings}");
+
+
+        }
+    public void DisplayRating()
+        {
+            switch(this.Ratings)
+            {
+                case MovieRatings.Excellent:
+                    Console.WriteLine($"Excellent Movie.Must Watch!");
+                    break;
+                case MovieRatings.Average:
+                    Console.WriteLine($"Not Bad.One Time Watch");
+                    break;
+                case MovieRatings.Bad:
+                    Console.WriteLine($"Bad");
+                    break;
+                case MovieRatings.Poor:
+                    Console.WriteLine($"Not Recommended");
+                    break;
+
+
+            }
+        }
+
+    }
+}
+
---
+++ MovieRating/Program.cs
@@ -0,0 +1,61 @@
+﻿using MovieRating;
+MovieRanking m1 = new MovieRanking(01, "Memories", 230, MovieRatings.Average);
+MovieRanking m2 = new MovieRanking(02, "SpiderMan", 230, MovieRatings.Excellent);
+MovieRanking m3 = new MovieRanking(03, "BKU", 230, MovieRatings.Excellent);
+MovieRanking m4 = new MovieRanking(04, "Asha", 230, MovieRatings.Poor);
+int choice;
+do
+{
+    Console.WriteLine("Movie Review");
+    Console.WriteLine("1.MEMORIES");
+    Console.WriteLine("2.SPIDERMAN");
+    Console.WriteLine("3.BKU");
+    Console.WriteLine("4.ASHA");
+    Console.WriteLine("Enter the choice you want");
+    choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            m1.DiplayDetails();
+            m1.DisplayRating();
+            break;
+        case 2:
+            m2.DiplayDetails();
+            m2.DisplayRating();
+            break;
+        case 3:
+            m3.DiplayDetails();
+            m3.DisplayRating();
+            break;
+        case 4:
+            m4.DiplayDetails();
+            m4.DisplayRating();
+            break;
+
+
+        case 5:
+            Console.WriteLine("end");
+            break;
+
+
+    }
+
+
+}while(choice != 5) ;
+
+
+//m1.DiplayDetails();
+//m1.DisplayRating();
+//Console.WriteLine();
+//m2.DiplayDetails();
+//m2.DisplayRating();
+//Console.WriteLine();
+//m3.DiplayDetails();
+//m3.DisplayRating();
+
+
+
+
+
+
