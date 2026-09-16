8515d9edf0787f6dbdc703cd9fbac1a1f68ad881
Anjana Rajan <anjana.rajan@wavetronix.com>
2026-09-16 17:47:34 +0530

    Commit the folder ProgrammingFundamentals

---
+++ ProgrammingFundamentals/MyFirstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyFirstProject;
+
+Console.WriteLine("Hello, World!");
+
+Student s1 = new Student();
+s1.rollnumber = 1;
+s1.name = "Anjana";
+s1.height = 149.2f;
+
+Console.WriteLine("Roll number:"+s1.rollnumber);
+Console.WriteLine("Name:"+s1.name);
+Console.WriteLine("height:" +s1.height);
+
+Console.ReadLine();
