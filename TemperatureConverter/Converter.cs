91362242b2d2cce2371865d8c98e05384b21fedc
Muhammed Adil C.P <muhammedadil.cp@wavetronix.com>
2026-09-16 22:30:16 +0530

    Temperature converter added

---
+++ TemperatureConverter/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    internal class Converter
+    {
+        public int Celsius { get; set; }
+
+        public float ConvertToFahrenheit(float celsius_value) { 
+            float fahrenheit_value = (celsius_value*9/5) + 32;
+            return fahrenheit_value;
+        }
+
+        public void PrintCelsiusAndFahrenheit(float fahrenheit_value, float celsius_value)
+        {
+            Console.WriteLine($"Celsius: {celsius_value}, Fahrenheit: {fahrenheit_value}");
+        }
+
+    }
+}
+
+
---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,7 @@
+﻿using TemperatureConverter;
+Console.WriteLine("Enter temperature in Celsius:");
+float celsiusvalue = Convert.ToSingle(Console.ReadLine());
+Converter c1 = new Converter();
+float fahrenheitvalue = c1.ConvertToFahrenheit(celsiusvalue);
+c1.PrintCelsiusAndFahrenheit(fahrenheitvalue, celsiusvalue);
+
