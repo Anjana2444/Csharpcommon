fad61f45e8f1da8584f4266e6fae96551a326868
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:53:43 +0530

    Initial commit

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿using TemperatureConverter;
+TempConverter t1 = new TempConverter();
+Console.WriteLine(" Enter the temperature in degree:");
+string val =Console.ReadLine();
+double celsius = double.Parse(val);
+Console.WriteLine($"The Temperature in degree is:{celsius}");
+double result = t1.tempConverter(celsius);
+Console.WriteLine($"The fahrenheit value is:{result}");
+
+
+
---
+++ TemperatureConverter/TeamperatureConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    internal class TempConverter
+    {
+
+        public double tempConverter(double celsius)
+        {
+            double Fahrenheit = celsius * 9.0 / 5.0 + 32;
+            return Fahrenheit;
+        }
+    }
+}
