1f79eba6a0edfddc1218e246b9d2556f8625e918
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:49:57 +0530

    Initial commit

---
+++ DemoApp/Demo/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    internal class Employee
+    {
+        public string name;
+        public int id;
+        public float salary;
+        public void show()
+        {
+            Console.WriteLine("This Function is used to display");
+        }
+
+        public Employee(string First_name, int id, float salary)
+        {
+            name = First_name;
+            this.id = id;
+            this.salary = salary;
+        }
+
+        internal class Department:Employee
+        {
+
+        }
+
+
+    }
+}
---
+++ DemoApp/Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿using Demo;
+
+Console.WriteLine("Hello, World!");
+Employee e1 = new Employee("dona",001,100000);
+Console.WriteLine("The details of the employee:\n" + "Name:"+e1.name +"\n"+"id:"+e1.id+"\n"+"salary:"+e1.salary);
+e1.show();
+
+Console.ReadLine();
+
