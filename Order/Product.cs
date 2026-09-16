15b4d2b398d784abe28a18968bb1cb21f4fb8c12
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:53:16 +0530

    Initial commit

---
+++ Order/Product.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order
+{
+    internal enum OrderStatus { OrderPlaced, Dispatched, Shipped, OutForDelivery }
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public DateTime DateOfOrder { get; set; }
+
+        public Product(int id, string name, int price, OrderStatus status, DateTime date_of_order)
+        {
+            this.ProductId = id;
+            this.Name = name;
+            this.Price = price;
+            this.Status = status;
+            this.DateOfOrder = date_of_order;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ProductId:{this.ProductId}");
+            Console.WriteLine($"Name:{this.Name}");
+            Console.WriteLine($"Price:{this.Price}");
+            Console.WriteLine($"Status:{this.Status}");
+            Console.WriteLine($"Date_Of_Order:{this.DateOfOrder}");
+        }
+
+        public void PrintCategory()
+        {
+            if (this.Price < 400)
+            {
+                Console.WriteLine("economic Product");
+            }
+            else if ((400 < this.Price) && (this.Price < 700))
+            {
+                Console.WriteLine("Mid Range");
+            }
+            else
+            {
+                Console.WriteLine("premimum");
+            }
+
+
+
+        }
+
+        public void PrintOrderStatus()
+        {
+            switch(this.Status)
+            {
+                case OrderStatus.Shipped:
+                    Console.WriteLine("Order is Shipped");
+                    break;
+
+                case OrderStatus.OrderPlaced:
+                    Console.WriteLine("Order is placed");
+                    break;
+                case OrderStatus.Dispatched:
+                    Console.WriteLine(" Order is Dispatched");
+                    break;
+                default:
+                    break;
+
+
+
+
+
+            }
+        }
+       
+
+
+        
+    }
+}
---
+++ Order/Program.cs
@@ -0,0 +1,120 @@
+﻿using Order;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+DateTime date = new DateTime(2026, 09, 10);
+
+
+Product p1 = new Product(101, "Book", 400, OrderStatus.Shipped, date);
+Product p2 = new Product(102, "Pen", 60, OrderStatus.OutForDelivery, date);
+Product p3 = new Product(103, "Paper", 10, OrderStatus.OutForDelivery, date);
+Product p4 = new Product(104, "Marker", 20, OrderStatus.OutForDelivery, date);
+Product p5 = new Product(105, "Pouch", 70, OrderStatus.OutForDelivery, date);
+//Array
+Product[] products = new[] { p1, p2, p3, p4, p5 };
+p1.Display();
+p1.PrintOrderStatus();
+//List
+List<Product> orders= new List<Product>();
+orders.Add(p1);
+orders.Add(p2);
+orders.Add(p3);
+orders.Add(p4);
+orders.Add(p5);
+
+// Dictionary
+Dictionary<int,Product> orderdict = new Dictionary<int, Product>();
+orderdict.Add(01, p1);
+orderdict.Add(02, p2);
+orderdict.Add(03, p3);
+orderdict.Add(04, p4);
+orderdict.Add(05, p5);
+orderdict.Remove(02);
+
+foreach (KeyValuePair<int,Product> o in orderdict)
+{
+    Console.WriteLine(o.Value.Name);
+    Console.WriteLine(o.Key);
+}
+
+for (int i=0; i < 10;i++)
+{ 
+    if (i == 5)
+    {
+        continue;
+    }
+    Console.WriteLine($"Number {i}");
+}
+
+for (int j = 0; j<(orders.Count);j++)
+{
+    if (j == 3)
+    {
+        break;
+    }
+    Console.WriteLine($"{j}");
+}
+ 
+
+//foreach (Product a in orderdict.Values)
+//{
+//    Console.WriteLine($"{a.Name}");
+//}
+
+
+
+
+
+
+
+
+
+
+
+//foreach (Product o in orders)
+//{
+//    Console.WriteLine(o.Name);
+//}
+
+//orders.Remove(p5);
+
+
+
+
+
+
+
+//for (int i = 0; i <= products.Length; i++)
+//{
+//    Console.WriteLine(products[i]);
+//}
+
+//foreach (Product p in products)
+//{
+//    Console.WriteLine(p.Name);
+//}
+
+
+
+
+
+// Loops
+//for(int i=0;i<10;i++)
+//{
+//    Console.WriteLine($"the numbers are{i}");
+//}
+//int i = 0;
+//while(i<10)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+////do
+//{
+//    Console.WriteLine(i);
+//    i++;
+//} while (i < 10)
+Console.ReadLine();
+
+
+
