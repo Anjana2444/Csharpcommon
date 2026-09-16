bd5cb6eeffb331ed029eaeb96ce2899d6baaed01
Muhammed Adil C.P <muhammedadil.cp@wavetronix.com>
2026-09-16 22:29:21 +0530

    School files added

---
+++ School/Program.cs
@@ -0,0 +1,28 @@
+﻿using School;
+Student s1=new Student(1, "John Doe", Gender.Male,Class.Sixth, 12);
+Student s2 = new Student(2, "Jane Smith", Gender.Female, Class.Seventh, 13);
+Student s3 = new Student(3, "Alex Johnson", Gender.Other, Class.Eighth, 14);
+Student s4 = new Student(4, "Emily Brown", Gender.Female, Class.Fourth, 10);
+Student s5 = new Student(5, "Michael Davis", Gender.Male, Class.Tenth, 15);
+Student s6 = new Student(6, "Sarah Wilson", Gender.Male, Class.Fifth, 11);
+Student s7 = new Student(7, "David Lee", Gender.Male, Class.Ninth, 14);
+Student s8 = new Student(8, "Olivia Taylor", Gender.Other, Class.Second, 8);
+Student s9 = new Student(9, "Daniel Anderson", Gender.Male, Class.Eighth, 13);
+Student s10 = new Student(10, "Sophia Martinez", Gender.Female, Class.Eighth, 14);
+
+Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+
+studentDictionary.Add(s1.StudentId, s1);
+studentDictionary.Add(s2.StudentId, s2);
+studentDictionary.Add(s3.StudentId, s3);
+studentDictionary.Add(s4.StudentId, s4);
+studentDictionary.Add(s5.StudentId, s5);
+studentDictionary.Add(s6.StudentId, s6);
+studentDictionary.Add(s7.StudentId, s7);
+studentDictionary.Add(s8.StudentId, s8);
+studentDictionary.Add(s9.StudentId, s9);
+studentDictionary.Add(s10.StudentId, s10);
+
+Student.ShowAddRemoveStudentInfo(studentDictionary);
+
+//Console.WriteLine("Hello, World!");
---
+++ School/Student.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School
+{
+    internal enum Gender { Male, Female, Other }
+    internal enum Class { First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth }
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public Gender StudentGender { get; set; }
+        public Class StudentClass { get; set; }
+        public int StudentAge { get; set; }
+
+        public Student(int studentid, string studentname, Gender studentgender, Class studentclass, int studentage)
+        {
+            this.StudentId = studentid;
+            this.StudentName = studentname;
+            this.StudentGender = studentgender;
+            this.StudentClass = studentclass;
+            this.StudentAge = studentage;
+
+        }
+
+        public static void ShowStudentInfo(Dictionary<int, Student> studentDictionary)
+        {
+            foreach (var student in studentDictionary.Values)
+            {
+                Console.WriteLine($"Student ID: {student.StudentId}, Student Name: {student.StudentName}, Gender: {student.StudentGender}, Class: {student.StudentClass}, Age: {student.StudentAge}");
+            }
+        }
+
+        public static void ShowAddRemoveStudentInfo(Dictionary<int, Student> studentDictionary)
+        {
+            int choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("1. Show All Students");
+                Console.WriteLine("2. Remove Student");
+                Console.WriteLine("3. Add Student");
+                Console.Write("Enter your choice: ");
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Please Enter a valid number");
+                    continue;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        ShowStudentInfo(studentDictionary);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Student ID to remove: ");
+                        if (!int.TryParse(Console.ReadLine(), out int idToRemove))
+                        {
+                            Console.WriteLine("Invalid Student ID!");
+                            continue;
+                        }
+                        if (studentDictionary.ContainsKey(idToRemove))
+                        {
+                            studentDictionary.Remove(idToRemove);
+                            Console.WriteLine("Student removed successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Student not found.");
+                        }
+                        break;
+
+                    case 3:
+                        Console.Write("Enter Student ID: ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Student Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter Gender (Male/Female/Other): ");
+                        if (!Enum.TryParse<Gender>(Console.ReadLine(),true, out Gender gender))
+                        {
+                            Console.WriteLine("Invalid Gender.");
+                            break;
+                        }
+                        Console.Write("Enter Class (First/Second/Third/Fourth/Fifth/Sixth/Seventh/Eighth/Ninth/Tenth): ");
+                        if(!Enum.TryParse<Class>(Console.ReadLine(),true, out Class className))
+                        {
+                            Console.WriteLine("Invalid Class.");
+                            break;
+                        }
+
+                        Console.Write("Enter Age: ");
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        studentDictionary.Add(id, new Student(id, name, gender, className, age));
+                        break;  
+                }
+            }
+        }
+    }
+}
