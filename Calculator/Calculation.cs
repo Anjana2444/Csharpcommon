d358aa646f9e89c77e00408e13746236f8a7ea0a
Dona Rose Shiju <donarose.shiju@wavetronix.com>
2026-09-16 17:43:51 +0530

    Initial Commit

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    internal class Calculation
+    {
+        public double addition(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double subtraction(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double multiplication(double x, double y)
+        {
+            return x * y;
+        }
+
+        public void division(double x, double y)
+        {
+             double val = x / y;
+            Console.WriteLine($"the division value is{val}");
+
+        }
+
+
+    }
+}
+
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Calculator;
+Calculation c1 = new Calculation();
+Console.WriteLine("Enter the first Number:");
+double first_num = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second Number:");
+double second_num = double.Parse(Console.ReadLine());
+double add_res = c1.addition(first_num, second_num);
+Console.WriteLine($"The addition result is:{add_res}");
+Console.WriteLine();
+double sub_res = c1.subtraction(first_num, second_num);
+Console.WriteLine($"The subtracted result is:{sub_res}");
+Console.WriteLine();
+double mul_res = c1.multiplication(first_num, second_num);
+Console.WriteLine($"The multiplication result is:{mul_res}");
+Console.WriteLine();
+Console.WriteLine();
+//double div_res = c1.division(first_num, second_num);
+//Console.WriteLine($"The division result is:{div_res}");
+//Console.WriteLine();
+c1.division(first_num, second_num);
+
+
+
+
+
+
+
