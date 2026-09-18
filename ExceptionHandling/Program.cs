7ba5b9d297b04531db0af9fec7b2027edd636349
Antony Jacob <antony.jacob@wavetronix.com>
2026-09-18 12:22:05 +0530

    Added file: ExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Try Block");
+    int a = 8;
+    int b = 0;
+    int c = a / b;
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("FileNotFoundException");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("DivideByZeroException");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Catch Block" + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally Block");
+}
+
+Console.ReadLine();
