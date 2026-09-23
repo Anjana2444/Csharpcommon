1e6d6a4747ce24760eb57a2d28d8866d5a733df4
Antony Jacob <antony.jacob@wavetronix.com>
2026-09-23 17:14:49 +0530

    Added File: 20260923_ReferenceType File and ConsoleApp File

---
+++ 20260923_ReferenceType/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Employee
+{
+    public int EmpId { get; set; }
+    public string Name { get; set; }
+}
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Employee e1 = new Employee { EmpId = 101, Name = "Antony" };
+        EmployeeValueType(e1);
+        Console.WriteLine($"Employee ID: {e1.EmpId}, Employee Name: {e1.Name}");
+        Employee e2 = new Employee { EmpId = 102, Name = "Jacob" };
+        EmployeeReferenceTypeRef(ref e2);
+        Console.WriteLine($"Employee ID: {e2.EmpId}, Employee Name: {e2.Name}");
+        Employee e3 = new Employee{ };
+        EmployeeReferenceTypeOut(out e3);
+        Console.WriteLine($"Employee ID: {e3.EmpId}, Employee Name: {e3.Name}");
+        Employee e4 = new Employee { EmpId = 104, Name = "Anns" };
+        EmployeeReferenceTypeIn(e4);
+        Console.WriteLine($"Employee ID: {e4.EmpId}, Employee Name: {e4.Name}");
+        Console.ReadLine();
+    }
+    public static void EmployeeValueType(Employee emp)
+    {
+        emp = new Employee();
+        emp.EmpId = 201;
+        emp.Name = "Chris";
+    }
+    public static void EmployeeReferenceTypeRef(ref Employee emp)
+    {
+        emp = new Employee();
+        emp.EmpId = 202;
+        emp.Name = "Joseph";
+    }
+    public static void EmployeeReferenceTypeOut(out Employee emp)
+    {
+        emp = new Employee();
+        emp.EmpId = 203;
+        emp.Name = "George";
+    }
+    public static void EmployeeReferenceTypeIn(in Employee emp)
+    {
+        //emp = new Employee();
+        emp.EmpId = 204;
+        emp.Name = "Adil";
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,99 @@
+﻿#region Call Type
+//Value Type
+void Value1(int val1)
+{
+    val1 = 100;
+}
+int num1 = 10;
+Value1(num1);
+
+//Refence Type using ref
+void Value2(ref int val2)
+{
+    val2 = 100;
+}
+int num2 = 10;
+Value2(ref num2);
+
+//Refence Type using out
+void Value3(out int val3)
+{
+    val3 = 100;
+}
+int num3;
+Value3(out num3);
+
+//Refence Type using in
+void Value4(in int val4)
+{
+    //val value can't be changed
+}
+int num4 = 10;
+Value4(num4);
+
+Console.WriteLine(num4);
+Console.ReadLine();
+#endregion
+#region Normal Delegate
+int Add(int x, int y)
+{
+    return x + y;
+}
+int Multiply(int x, int y)
+{
+    return x * y;
+}
+void DisplayResult(Calculate calculate, int x, int y)
+{
+    Console.WriteLine(calculate(x, y));
+}
+
+Calculate add_calculate = Add;
+Calculate mul_calculate = Multiply;
+
+DisplayResult(add_calculate, 5, 7);
+DisplayResult(mul_calculate, 5, 9);
+
+Console.ReadLine();
+#endregion
+#region Tuple Type Function
+(string, int) MultipleReturn() // Returning multiple data type values values
+{
+    return ("Antony", 21);
+}
+string name = MultipleReturn().Item1;
+int age = MultipleReturn().Item2;
+(string name1, int age1) = MultipleReturn();
+var full_details = MultipleReturn();
+Console.WriteLine($"Name: {name}, Age: {age}");
+Console.WriteLine($"Name: {name1}, Age: {age1}");
+Console.WriteLine($"Full Details: {full_details}");
+Console.ReadLine();
+#endregion
+#region Name Normal Delegate
+string CombineString(string first_name, string last_name)
+{
+    return first_name + " " + last_name;
+}
+CombineDel combining = CombineString;
+string full_name = combining("1. Antony", "Jacob");
+Console.WriteLine(full_name);
+Console.ReadLine();
+#endregion
+#region Name Anonymous Delegate
+CombineDel combining1 = delegate (string first_name1, string last_name1) // Anonymous Delegate
+{
+    return first_name1 + " " + last_name1;
+};
+string full_name1 = combining1("2. Antony", "Jacob");
+Console.WriteLine(full_name1);
+Console.ReadLine();
+#endregion
+#region Name Lamda Anonymous Delegate
+CombineDel combining2 = (first_name2, last_name2) =>first_name2 + " " + last_name2; // Lmada Expression
+string full_name2 = combining2("3. Antony", "Jacob");
+Console.WriteLine(full_name2);
+Console.ReadLine();
+public delegate string CombineDel(string x, string y);
+public delegate int Calculate(int val1, int val2);
+#endregion
